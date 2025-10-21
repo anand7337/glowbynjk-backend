f70d4ad7802370113418c000220b96627ab18216
Anand S <109635488+anand7337@users.noreply.github.com>
2025-10-21 08:44:34 +0530

    Update userLogin.js

--- controller/userLogin.js
+++ controller/userLogin.js
@@ -644,9 +644,9 @@ const deleteGiftCard = async (req, res) => {
 
     // Send confirmation email (optional)
     const transporter = nodemailer.createTransport({
-host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+ host: "smtp.gmail.com",
+  port: 587,
+  secure: false, 
         auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
